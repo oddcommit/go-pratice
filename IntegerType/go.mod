8cada28f2f133444768567ef8c052d12d3a4f51c
kristoff <leeweiming524@gmail.com>
2023-10-28 15:20:32 +0900

    understanding integer type

---
+++ IntegerType/go.mod
@@ -0,0 +1,3 @@
+module example.com/integertype
+
+go 1.21.3
---
+++ IntegerType/integertype.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integer1 int
+	var integer2 int
+
+	integer1 = 5
+	integer2 = 10
+
+	fmt.Println(integer1)
+	fmt.Print((integer2))
+}
